92efc980db2421d021decc2a3be2d862ca209b64
oldsn <oldsn.ck@msa.hinet.net>
2015-06-24 15:10:32 +0800

    Initial commit

---
+++ app/src/main/java/idv/u97/helloapp/Cube.java
@@ -0,0 +1,16 @@
+package idv.u97.helloapp;
+
+/**
+ * Created by 201 on 2015/6/24.
+ */
+public class Cube {
+    private int length;
+    private int width;
+    private int height;
+
+    public Cube(int length, int width, int height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+}
